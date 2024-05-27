76d36945c40ff8a2f52b409db963d718a2461653
rouge9 <robelged1992@gmail.com>
2024-05-27 10:08:27 -0900

    app logo changed

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,11 @@ import 'package:expense_monitor/app.dart';
 import 'package:expense_monitor/simple_bloc_observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:user_repository/user_repository.dart';
 
 void main() async {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   Bloc.observer = SimpleBlocObserver();
-  FlutterNativeSplash.remove();
   runApp(MyApp(FirebaseUserRepo()));
 }
